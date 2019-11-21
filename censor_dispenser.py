bc1494e307dbbd4cb431ac68614aa1c884ba5b38
Christopher Lamm <chris@theantichris.com>
2019-11-21 09:14:17 -0500

    function that censors a phrase in given text

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -2,3 +2,7 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+def censor_text(text, censored_phrase):
+    return text.replace(censored_phrase, "RECACTED")
+
