af1681d534dbe058c7dd0ff441c5dc2cf6bdbc41
Christopher Lamm <chris@theantichris.com>
2019-11-21 09:20:57 -0500

    function to censor multiple phrases

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,6 +3,16 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-def censor_text(text, censored_phrase):
-    return text.replace(censored_phrase, "RECACTED")
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
+def censor_phrase(text, phrase):
+    return text.replace(phrase, "RECACTED")
+
+print(censor_phrase(email_one, proprietary_terms[4]))
+
+def censor_phrases(text, phrases):
+    for phrase in phrases:
+        text = censor_phrase(text, phrase)
+    return text
+
+print(censor_phrases(email_two, proprietary_terms))
